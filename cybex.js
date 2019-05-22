17e96ff50f5b7ffe464a1c421eef5a2f319f8591
shantilu <ruoshuilu1990@gmail.com>
2019-05-22 17:55:06 +0800

    account setup

--- cybex.js
+++ cybex.js
@@ -23,13 +23,13 @@ class CybexSigner {
 
         this.availableAssets = [];
         this.availableAssetPairs = [];
-        this.user={};
+        this.user = {};
         this.has_crendential = false;
     }
 
     executeRestRequest(url, method = 'GET', data = undefined) {
-        return fetch(url, { method: method, body: JSON.stringify(data), headers: {'Content-type': 'application/json'} })
-            .catch(error=>{
+        return fetch(url, {method: method, body: JSON.stringify(data), headers: {'Content-type': 'application/json'}})
+            .catch(error => {
                 console.log(error);
             })
             .then(res => res.json());
@@ -44,38 +44,35 @@ class CybexSigner {
         this.availableAssetPairs = availableAssetPairs;
     }
 
-    async set_credential(account, key, uat=false){
+    set_credential(account, key, pubKeyRes) {
 
-        if(account && key){
+        if (account && key) {
             const privateKey = PrivateKey.fromWif(key);
-            if(uat){
-                return this.set_valid_crendential(account, privateKey);
-            }
 
-            const data = {"method":"call","params":[0,"get_objects",[[account]]],"id":2};
-            const _Res = await this.executeRestRequest("https://apihk.cybex.io/", "POST", data);
-            const pubKeyRes = _Res.result[0];
-
-            if(pubKeyRes.id === account){
+            if (pubKeyRes) {
                 // check active key
-                pubKeyRes.active.key_auths.forEach(pubkey=>{
+                pubKeyRes.active.key_auths.forEach(pubkey => {
 
-                    if(pubkey[0] === privateKey.toPublicKey().toString()){
+                    if (pubkey[0] === privateKey.toPublicKey().toString()) {
                         this.set_valid_crendential(account, privateKey);
+                        return true
                     }
                 })
                 // check owner key
-                pubKeyRes.owner.key_auths.forEach(pubkey=>{
+                pubKeyRes.owner.key_auths.forEach(pubkey => {
 
-                    if(pubkey[0] === privateKey.toPublicKey().toString()){
+                    if (pubkey[0] === privateKey.toPublicKey().toString()) {
                         this.set_valid_crendential(account, privateKey);
+                        return true
                     }
                 })
             }
         }
+
+        return false;
     }
 
-    set_valid_crendential(account, privateKey){
+    set_valid_crendential(account, privateKey) {
         this.pKey = privateKey;
         this.user = {id: account};
         this.has_crendential = true;
@@ -109,7 +106,7 @@ class CybexSigner {
 
     }
 
-    op_sign(type_name, obj, trx_id) {
+    op_sign(type_name, obj, trx_id, isBuy) {
 
         let tr = new TransactionBuilder();
 
@@ -135,8 +132,8 @@ class CybexSigner {
                 minToReceive: {"assetId": obj.min_to_receive.asset_id, amount: obj.min_to_receive.amount},
                 expiration: obj.expiration,
                 signature: sig,
-                fill_or_kill: obj.fill_or_kill?1:0,
-                isBuy: obj.isBuy
+                fill_or_kill: obj.fill_or_kill ? 1 : 0,
+                isBuy: isBuy
             };
 
         } else if (type_name === "limit_order_cancel") {
@@ -185,11 +182,11 @@ class CybexSigner {
     limit_order_create(pair, side, price, amount, total = null) {
         // console.log(side, "price:",price,"amount", amount);
 
-        if(!this.has_crendential) {
+        if (!this.has_crendential) {
             throw new Error("You need to provide valid credentials for signing")
         }
 
-        if(typeof pair === 'string'){
+        if (typeof pair === 'string') {
             pair = this.get_pair(pair)
         }
 
@@ -199,7 +196,7 @@ class CybexSigner {
             end.setHours(23, 59, 59, 999);
             const exp = Math.floor(end / 1000) + 1;
 
-            let isBuy=0;
+            let isBuy = 0;
 
             // calculate buy sell
             let sell, receive;
@@ -229,17 +226,16 @@ class CybexSigner {
             let obj = {
                 "seller": this.user.id,
                 "amount_to_sell": sell,
-                "min_to_receive": receive  ,
+                "min_to_receive": receive,
                 "expiration": exp,
                 "fill_or_kill": false,
                 "fee": {
                     "amount": 55,
                     "asset_id": this.fee_asset_id
-                },
-                "isBuy": isBuy
+                }
             };
 
-            return this.op_sign("limit_order_create", obj);
+            return this.op_sign("limit_order_create", obj, undefined, isBuy);
 
         } catch (e) {
             console.error(e)
@@ -247,7 +243,7 @@ class CybexSigner {
     }
 
     limit_order_cancel(trx_id) {
-        if(!this.has_crendential) {
+        if (!this.has_crendential) {
             throw new Error("You need to provide valid credentials for signing")
         }
         try {
@@ -270,10 +266,10 @@ class CybexSigner {
     }
 
     cancel_all(pair) {
-        if(!this.has_crendential) {
+        if (!this.has_crendential) {
             throw new Error("You need to provide valid credentials for signing")
         }
-        if(typeof pair === 'string'){
+        if (typeof pair === 'string') {
             pair = this.get_pair(pair)
         }
 
@@ -298,70 +294,83 @@ class CybexSigner {
 class Cybex {
     constructor(config) {
         this.loaded = false;
-        this.signer = new CybexSigner();
-
-        //accountName = undefined, account = undefined, key = undefined, environ = "prod", verbose=true
         this.apiEndPoint = "https://api.cybex.io/v1/";
-
-        if(config && config.environ && config.environ==='uat'){
-            this.apiEndPoint = "https://apitest.cybex.io/v1/";
-            this.uat=true;
-        }
-        if(config && config.accountName){
-            this.accountName = config.accountName
-        }
+        this.chainEndPoint = "https://hongkong.cybex.io/";
+        this.verbose=true;
+        this.signer = new CybexSigner();
         this.config = config;
+
+        this.setEnviron(config);
     }
-    
+
+    setEnviron(config){
+
+        if (config && config.environ && config.environ === 'uat') {
+            this.apiEndPoint = "https://apitest.cybex.io/v1/";
+            this.chainEndPoint = "http://18.136.140.223:38090";
+        }
+    }
+
     executeRestRequest(url, method = 'GET', data = undefined) {
-        if(this.verbose&&data){
+        console.log(url);
+        console.log(data);
+        if (this.verbose && data) {
             console.log(data);
         }
-        return fetch(url, { method: method, body: JSON.stringify(data), headers: {'Content-type': 'application/json'} })
-            .catch(error=>{
+        return fetch(url, {method: method, body: JSON.stringify(data), headers: {'Content-type': 'application/json'}})
+            .catch(error => {
                 console.log(error);
+                if (data.transactionId) {
+                    return data.transactionId;
+                }
             })
             .then(res => res.json());
     }
-    async setSigner(config){
-        this.verbose = config?config.verbose:this.verbose?this.verbose:true;
-        this.enableRateLimit = config?config.enableRateLimit:this.enableRateLimit;
 
-        if(config && config.environ && config.environ==='uat'){
-            this.apiEndPoint = "https://apitest.cybex.io/v1/";
-            this.uat=true;
-        }
+    async setSigner(config) {
+        this.verbose = config ? config.verbose : this.verbose ? this.verbose : true;
+        this.enableRateLimit = config ? config.enableRateLimit : this.enableRateLimit;
+        this.setEnviron(config);
 
-        if(!config.account && config.accountName){
-            const data = {"method": "call", "params": [0, "lookup_accounts",[config.accountName, 50]], "id": 1};
-            const prod_chain_endpoint = "https://hongkong.cybex.io/";
+        if (!config.account && config.accountName) {
+            const data = {"method": "call", "params": [0, "lookup_accounts", [config.accountName, 50]], "id": 1};
 
-            const res = await this.executeRestRequest(prod_chain_endpoint, "POST", data);
-            if(res.result.length&&res.result[0].length>1){
+            const res = await this.executeRestRequest(this.chainEndPoint, "POST", data);
+            if (res.result.length && res.result[0].length > 1) {
                 config.account = res.result[0][1]
             }
         }
 
-        if(!config.key && config.accountName && config.password){
-            const genedKey = generateKeys(config.accountName,config.password)
-            config.key = genedKey.toWif()
-        }
+        if (config.account) {
+            const data = {"method": "call", "params": [0, "get_objects", [[config.account]]], "id": 2};
 
-        if(config.account&&config.key){
-            const result =  await this.signer.set_credential(config.account, config.key, this.uat);
-            if (!this.signer.has_crendential){
-                console.log("private key is not valid")
+            const res = await this.executeRestRequest(this.chainEndPoint, "POST", data);
+            if (res.result.length) {
+                const accountInfo = res.result[0];
+                config.accountName = accountInfo.name;
+                this.accountName = accountInfo.name;
+
+                if (!config.key && config.accountName && config.password) {
+                    const genedKey = generateKeys(config.accountName, config.password)
+                    config.key = genedKey.toWif()
+                }
+
+                if (config.key) {
+                    this.signer.set_credential(config.account, config.key, accountInfo);
+                }
+
+                if (!this.signer.has_crendential) {
+                    console.log("private key is not valid")
+                }
             }
+        }else{
+            console.log("account is not valid")
         }
-        else{
-            console.log("account or/and private key is not valid")
-        }
-
     }
 
     async get_pair(assetPair) {
 
-        if(!this.loaded){
+        if (!this.loaded) {
             const res = await this.loadMarkets()
         }
 
@@ -391,8 +400,8 @@ class Cybex {
 
     }
 
-    async loadMarkets(reload=false) {
-        if(reload||!this.loaded){
+    async loadMarkets(reload = false) {
+        if (reload || !this.loaded) {
             return await this.fetchMarkets();
         }
     }
@@ -407,7 +416,7 @@ class Cybex {
             this.availableAssets = res.availableAssets;
             this.availableAssetPairs = res.availableAssetPairs;
 
-            this.signer.set_chain_params(res.chainId, res.refBlockId, res.availableAssets, res.availableAssetPairs );
+            this.signer.set_chain_params(res.chainId, res.refBlockId, res.availableAssets, res.availableAssetPairs);
 
             this.loaded = true;
         });
@@ -415,70 +424,68 @@ class Cybex {
         return this.availableAssetPairs
     }
 
-    async fetchOrderBook(assetPair, limit=3) {
-        const url = this.apiEndPoint + "orderBook?assetPair="+assetPair+"&limit="+limit;
+    async fetchOrderBook(assetPair, limit = 3) {
+        const url = this.apiEndPoint + "orderBook?assetPair=" + assetPair + "&limit=" + limit;
 
         return await this.executeRestRequest(url)
     }
 
-    async fetchOHLCV(assetPair, interval="1h", limit=10) {
+    async fetchOHLCV(assetPair, interval = "1h", limit = 10) {
 
-        const url = this.apiEndPoint + "klines?assetPair="+assetPair+"&interval="+interval+"&limit="+10;
+        const url = this.apiEndPoint + "klines?assetPair=" + assetPair + "&interval=" + interval + "&limit=" + 10;
         return await this.executeRestRequest(url)
 
     }
 
-    async fetchTrades(assetPair, reverse=true, limit=10) {
-        const url = this.apiEndPoint + "trade?assetPair="+assetPair+"&reverse="+reverse+"&limit="+limit;
+    async fetchTrades(assetPair, reverse = true, limit = 10) {
+        const url = this.apiEndPoint + "trade?assetPair=" + assetPair + "&reverse=" + reverse + "&limit=" + limit;
         return await this.executeRestRequest(url)
     }
 
     // Private
 
     async fetchBalance(accountName) {
-        const accName = accountName?accountName:this.accountName;
-        if(accName){
-            const url = this.apiEndPoint + "position?accountName="+accName;
+        const accName = accountName ? accountName : this.accountName;
+        if (accName) {
+            const url = this.apiEndPoint + "position?accountName=" + accName;
             return await this.executeRestRequest(url)
         }
         return [];
 
     }
 
-    async fetchBestPrice(assetPair){
+    async fetchBestPrice(assetPair) {
         const orderbook = await this.fetchOrderBook(assetPair, 1);
-        return {bid:orderbook.bids[0][0], ask:orderbook.asks[0][0]}
+        return {bid: orderbook.bids[0][0], ask: orderbook.asks[0][0]}
 
     }
 
     async createOrder(assetPair, side, amount, price) {
 
-        if(this.signer.has_crendential){
-            if(!this.loaded){
+        if (this.signer.has_crendential) {
+            if (!this.loaded) {
                 const res = await this.loadMarkets()
             }
 
             const parsed = await this.get_pair(assetPair);
-            const total = amount*price;
+            const total = amount * price;
 
-            if( parsed.minQuantity && (amount<parsed.minQuantity)){
-                console.log("Mininum quantity is "+parsed.minQuantity);
+            if (parsed.minQuantity && (amount < parsed.minQuantity)) {
+                console.log("Mininum quantity is " + parsed.minQuantity);
                 return
             }
-            if( parsed.minTickSize && (total<parsed.minTickSize)){
-                console.log("Mininum minTickSize is "+parsed.minQuantity);
+            if (parsed.minTickSize && (total < parsed.minTickSize)) {
+                console.log("Mininum minTickSize is " + parsed.minQuantity);
                 return
             }
             const signedTx = this.signer.limit_order_create(parsed, side, price, amount, total);
-            //signedTx.isBuy = side==="buy"?1:0;
+
             const url = this.apiEndPoint + "transaction";
 
             const res = await this.executeRestRequest(url, "POST", signedTx);
-            if(res.Status === 'Successful'){
-                res.transactionId=signedTx.transactionId;
-                return res;
-            }
-            console.log(res);
+
+            res.transactionId = signedTx.transactionId;
+            return res;
         }
     }
 
@@ -493,18 +500,18 @@ class Cybex {
 
     async createMarketBuyOrder(assetPair, amount) {
         const bestPrice = await this.fetchBestPrice(assetPair);
-        return this.createOrder(assetPair, "buy", amount, bestPrice.ask*1.01);
+        return this.createOrder(assetPair, "buy", amount, bestPrice.ask * 1.01);
     }
 
     async createMarketSellOrder(assetPair, amount) {
         const bestPrice = await this.fetchBestPrice(assetPair);
-        return this.createOrder(assetPair, "sell", amount, bestPrice.bid*0.99);
+        return this.createOrder(assetPair, "sell", amount, bestPrice.bid * 0.99);
     }
 
     async cancelOrder(trx_id) {
-        if(this.signer.has_crendential){
+        if (this.signer.has_crendential) {
 
-            if(!this.loaded){
+            if (!this.loaded) {
                 const res = await this.loadMarkets()
             }
 
@@ -517,8 +524,8 @@ class Cybex {
     }
 
     async cancelAll(assetPair) {
-        if(this.signer.has_crendential){
-            if(!this.loaded){
+        if (this.signer.has_crendential) {
+            if (!this.loaded) {
                 const res = await this.loadMarkets()
             }
 
@@ -532,44 +539,44 @@ class Cybex {
     }
 
     async fetchOrder(trx_id, accountName) {
-        const accName = accountName?accountName:this.accountName;
-        if(accName){
-            const url = this.apiEndPoint + "order?accountName="+accName+"&transactionId="+trx_id;
+        const accName = accountName ? accountName : this.accountName;
+        if (accName) {
+            const url = this.apiEndPoint + "order?accountName=" + accName + "&transactionId=" + trx_id;
             return await this.executeRestRequest(url)
         }
     }
 
-    async fetchOrders(assetPair, accountName, reverse=true) {
-        const accName = accountName?accountName:this.accountName;
-        if(accName){
-            const _pairStr = assetPair?"&assetPair="+assetPair:"";
-            const url = this.apiEndPoint + "order?accountName="+accName+_pairStr+"&reverse="+reverse;
+    async fetchOrders(assetPair, accountName, reverse = true) {
+        const accName = accountName ? accountName : this.accountName;
+        if (accName) {
+            const _pairStr = assetPair ? "&assetPair=" + assetPair : "";
+            const url = this.apiEndPoint + "order?accountName=" + accName + _pairStr + "&reverse=" + reverse;
             return await this.executeRestRequest(url)
         }
     }
 
-    async fetchOpenOrders(assetPair, accountName, reverse=true) {
-        const accName = accountName?accountName:this.accountName;
-        if(accName){
-            const _pairStr = assetPair?"&assetPair="+assetPair:"";
-            const url = this.apiEndPoint + "order?accountName="+accName +_pairStr+"&orderStatus=OPEN, PENDING_NEW, PENDING_CXL"+"&reverse="+reverse;
+    async fetchOpenOrders(assetPair, accountName, reverse = true) {
+        const accName = accountName ? accountName : this.accountName;
+        if (accName) {
+            const _pairStr = assetPair ? "&assetPair=" + assetPair : "";
+            const url = this.apiEndPoint + "order?accountName=" + accName + _pairStr + "&orderStatus=OPEN, PENDING_NEW, PENDING_CXL" + "&reverse=" + reverse;
             return await this.executeRestRequest(url)
         }
     }
 
-    async fetchClosedOrders(assetPair, accountName, reverse=true) {
-        const accName = accountName?accountName:this.accountName;
-        if(accName){
-            const url = this.apiEndPoint + "order?accountName="+accName+_pairStr+"&orderStatus=FILLED, CANCELED, REJECTED"+"&reverse="+reverse;
+    async fetchClosedOrders(assetPair, accountName, reverse = true) {
+        const accName = accountName ? accountName : this.accountName;
+        if (accName) {
+            const url = this.apiEndPoint + "order?accountName=" + accName + _pairStr + "&orderStatus=FILLED, CANCELED, REJECTED" + "&reverse=" + reverse;
             return await this.executeRestRequest(url)
         }
     }
 
-    async fetchMyTrades(assetPair, accountName, reverse=true) {
-        const accName = accountName?accountName:this.accountName;
-        if(accName){
-            const _pairStr = assetPair?"&assetPair="+assetPair:"";
-            const url = this.apiEndPoint + "trade?accountName="+accName+_pairStr+"&reverse="+reverse;
+    async fetchMyTrades(assetPair, accountName, reverse = true) {
+        const accName = accountName ? accountName : this.accountName;
+        if (accName) {
+            const _pairStr = assetPair ? "&assetPair=" + assetPair : "";
+            const url = this.apiEndPoint + "trade?accountName=" + accName + _pairStr + "&reverse=" + reverse;
             return await this.executeRestRequest(url)
         }
     }
